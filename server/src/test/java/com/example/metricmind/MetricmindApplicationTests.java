541f48bfa0da612587a7ff1d498b13b841d52376
NikitaBerezhnyj <nikita.berezhnyj@gmail.com>
2026-01-21 00:42:02 +0200

    chore: initialize server in spring boot

---
+++ server/src/test/java/com/example/metricmind/MetricmindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.metricmind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetricmindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
