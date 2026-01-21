c166c18503578bc1819007eeedf563bdeaccd182
NikitaBerezhnyj <nikita.berezhnyj@gmail.com>
2026-01-21 11:33:42 +0200

    refactor: renamed main backend files

--- server/src/main/java/com/example/metricmind/MetricmindApplication.java
+++ server/src/main/java/com/example/metricmind/MetricmindApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MetricmindApplication {
+public class MetricMindApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MetricmindApplication.class, args);
+		SpringApplication.run(MetricMindApplication.class, args);
 	}
 
 }
